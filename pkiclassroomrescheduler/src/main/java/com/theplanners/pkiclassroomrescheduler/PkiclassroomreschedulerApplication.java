e45717f020865f7373155ed4e2d065a497346934
Evan <evanriley@unomaha.edu>
2024-02-15 17:46:48 -0600

    Initilized project with spring boot initilizer

---
+++ pkiclassroomrescheduler/src/main/java/com/theplanners/pkiclassroomrescheduler/PkiclassroomreschedulerApplication.java
@@ -0,0 +1,13 @@
+package com.theplanners.pkiclassroomrescheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkiclassroomreschedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkiclassroomreschedulerApplication.class, args);
+	}
+
+}
---
+++ pkiclassroomrescheduler/src/test/java/com/theplanners/pkiclassroomrescheduler/PkiclassroomreschedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theplanners.pkiclassroomrescheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkiclassroomreschedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
