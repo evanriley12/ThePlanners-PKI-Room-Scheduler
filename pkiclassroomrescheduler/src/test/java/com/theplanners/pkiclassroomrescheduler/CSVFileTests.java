29c7a8b811604ba8643fce70c0b0e0e53efe92ec
Shane Ellis <shaneellis@unomaha.edu>
2024-03-21 16:55:43 -0500

    Stubbed Out Initial CSV File Related Tests

---
+++ pkiclassroomrescheduler/src/test/java/com/theplanners/pkiclassroomrescheduler/CSVFileTests.java
@@ -0,0 +1,20 @@
+package com.theplanners.pkiclassroomrescheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CSVFileTests {
+    
+    // Test to check whether file upload from frontend was successful
+    @Test
+    void fileUploads() throws Exception {
+
+    }
+
+    // Test to check whether file parsing from backend was successful
+    @Test
+    void fileParses() throws Exception {
+
+    }
+}
