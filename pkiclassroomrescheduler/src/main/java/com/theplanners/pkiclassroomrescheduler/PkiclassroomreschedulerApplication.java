87169a47cb3337690f2f2c141e89f76bcf6df753
Evan <evanriley@unomaha.edu>
2024-03-21 17:25:55 -0500

    Read in classroom list at application boot

--- pkiclassroomrescheduler/src/main/java/com/theplanners/pkiclassroomrescheduler/PkiclassroomreschedulerApplication.java
+++ pkiclassroomrescheduler/src/main/java/com/theplanners/pkiclassroomrescheduler/PkiclassroomreschedulerApplication.java
@@ -2,12 +2,17 @@ package com.theplanners.pkiclassroomrescheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.theplanners.pkiclassroomrescheduler.system.ReadClassroom;
 
 @SpringBootApplication
 public class PkiclassroomreschedulerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PkiclassroomreschedulerApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(PkiclassroomreschedulerApplication.class, args);
+		ReadClassroom readClassroom = applicationContext.getBean(ReadClassroom.class);
+		readClassroom.readClassroomCSV();
 	}
 
 }
