2493721f910880ed1ed615bfcc0e8cb89397e2bf
Ben Monday <bmonday@unomaha.edu>
2024-04-02 17:36:21 -0500

    Start of the Algorithm

--- pkiclassroomrescheduler/src/main/java/com/theplanners/pkiclassroomrescheduler/system/Algorithm.java
+++ pkiclassroomrescheduler/src/main/java/com/theplanners/pkiclassroomrescheduler/system/Algorithm.java
@@ -11,7 +11,7 @@ public class Algorithm {
         this.classroomList = classroomList;
     }
 
-    public String doAlgorithm(Section section, int newSize) {
+    public String doAlgorithmOld(Section section, int newSize) {
         int currentRoom = section.getRoomNumber();
         ArrayList<Classroom> classrooms = classroomList.returnClassrooms();
         ArrayList<String> newRooms = new ArrayList<String>();
@@ -21,5 +21,31 @@ public class Algorithm {
             }
         }
         return newRooms.toString();
+    }
+
+    public String doAlgorithm(Section section, int newSize) {
+        // Get all classrooms
+        ArrayList<Classroom> allClassrooms = classroomList.returnClassrooms();
+        // Filter out classrooms that will not work
+        ArrayList<Classroom> possibleClassrooms = new ArrayList<Classroom>();
+        // Iterate through every classroom
+        for (int i = 0; i < allClassrooms.size(); i++) {
+            // Check if the classroom being checked is the sections current class or if it is too small to accomodate the new size. 
+            // If either is true, move on, otherwise, add the classroom to the list of potential classrooms.
+            if (section.getRoomNumber() == allClassrooms.get(i).getRoom() || allClassrooms.get(i).getSeats() < newSize) {
+                continue;
+            } else {
+                possibleClassrooms.add(allClassrooms.get(i));
+            }
+        }
+        // Check if the list of possible classrooms is empty before continuing.
+        if (possibleClassrooms.size() <= 0) {
+            return null;
+        }
+        // Now with a list of potential rooms, see if any of the rooms are not in use by an adjacent node.
+        for (int i = 0; i < possibleClassrooms.size(); i++) {
+            // TODO: Get adjacent classes from the schedule and check classrooms. 
+        }
+        return "";
     }
 }
