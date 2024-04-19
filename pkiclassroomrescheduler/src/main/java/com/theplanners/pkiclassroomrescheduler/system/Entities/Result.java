57b19f2b3a025664b078d9536b917586438093b1
Ben Monday <bmonday@unomaha.edu>
2024-04-18 19:29:51 -0500

    Updated result error message

--- pkiclassroomrescheduler/src/main/java/com/theplanners/pkiclassroomrescheduler/system/Entities/Result.java
+++ pkiclassroomrescheduler/src/main/java/com/theplanners/pkiclassroomrescheduler/system/Entities/Result.java
@@ -82,7 +82,7 @@ public class Result {
     public String toString() {
         // Print error if anything is null
         if (course == null || courseTitle == null || oldClassroom == null || newClassroom == null) {
-            return "Rescheduling could not be completed. The course may be unable to move from it's current classroom, or no classroom is big enough to accomodate the increased size. Please select a different course.";
+            return "Rescheduling could not be completed. The course may be unable to move from it's current classroom, or no classroom is big enough to accomodate the increased size.";
         }
         String equalString = "";
         String worseString = "";
